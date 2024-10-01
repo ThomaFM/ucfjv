c73c791be524f5d93889e02d8279efeb1266731b
Brian G <130684373+azurerazor@users.noreply.github.com>
2024-10-01 19:02:35 -0400

    2D Prefix Sum (#20)
    * add 2d pref sum
    
    * Delete content/data-structures/SubMatrix.h
    
    * add new lines to 2dps and 2col

---
+++ content/data-structures/PS2D.h
@@ -0,0 +1,29 @@
+/**
+ * Author: Brian Grana
+ * Date: 2024-09-30
+ * Source: own
+ * Description: Calculates sub-rectangle sums in O(1) time.
+ * Query function gets subgrid sum on any elements with row
+ * [r1, r2) and column [c1, c2).
+ * Time: O(NM + Q)
+ * Status: Tested on https://cses.fi/problemset/task/1652
+ */
+#pragma once
+
+struct PS2D {
+	vector<vector<long long>> ps;
+	int n, m;
+	PS2D(vector<vector<long long>>& a) {
+		n = size(a), m = size(a[0]);
+		ps = vector(n+1, vector<long long>(m+1));
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				ps[i+1][j+1] = a[i][j] + ps[i][j+1]
+				 + ps[i+1][j] - ps[i][j];
+			}
+		}
+	}
+	long long query(int r1, int c1, int r2, int c2) {
+		return ps[r2][c2] - ps[r1][c2] - ps[r2][c1] + ps[r1][c1];
+	}
+};
