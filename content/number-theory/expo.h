78135080e841391a8735c63e7db65b7f4a82074b
Brian G <130684373+azurerazor@users.noreply.github.com>
2024-10-02 17:10:53 -0400

    Create expo.h

---
+++ content/number-theory/expo.h
@@ -0,0 +1,21 @@
+/**
+ * Author: Brian Grana
+ * Date: 2024-10-02
+ * Source: own
+ * Description: Given two integers b and p, computes b
+ * raised to the power of p under modulo.
+ * Time: $O(log(p))$
+ * Status: tested on https://cses.fi/problemset/task/1095/
+ */
+#pragma once
+
+const long long mod = 1e9+7;
+long long expo(long long b, long long p) {
+    long long ans = 1;
+    while (p > 0) {
+        if (p & 1) ans = (ans * b) % mod;
+        b = (b * b) % mod;
+        p >>= 1;
+    }
+    return ans;
+}
