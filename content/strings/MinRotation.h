89271a45e64947d4bfa4212c9034f3eb0f190d99
enwask <enwask@ucf.edu>
2024-10-03 16:50:22 -0400

    Expand macros again but without parentheses

--- content/strings/MinRotation.h
+++ content/strings/MinRotation.h
@@ -12,7 +12,7 @@
 
 int minRotation(string s) {
 	int a=0, N=sz(s); s += s;
-	rep(b,0,N) rep(k,0,N) {
+	for (int b = 0; b < N; b++) for (int k = 0; k < N; k++) {
 		if (a+k == b || s[a+k] < s[b+k]) {b += max(0, k-1); break;}
 		if (s[a+k] > s[b+k]) { a = b; break; }
 	}
