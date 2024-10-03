0d14d84b01dd4d9cd6a0c726f25e404401c404f5
Brian G <130684373+azurerazor@users.noreply.github.com>
2024-10-03 00:31:57 -0400

    sv -> isPrime

--- content/number-theory/Eratosthenes.h
+++ content/number-theory/Eratosthenes.h
@@ -10,10 +10,10 @@
 #pragma once
 
 vector<bool> sieve(int n) {
-	vector<bool> sv(n+1, 1);
-	sv[0] = sv[1] = 0;
-	for (int i = 2; i * i <= n; i++) if (sv[i]) {
-		for (int j = i * i; j <= n; j += i) sv[j] = 0;
+	vector<bool> isPrime(n+1, 1);
+	isPrime[0] = isPrime[1] = 0;
+	for (int i = 2; i * i <= n; i++) if (isPrime[i]) {
+		for (int j = i * i; j <= n; j += i) isPrime[j] = 0;
 	}
-	return sv;
+	return isPrime;
 }
