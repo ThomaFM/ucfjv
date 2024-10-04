d89f06d241a5cdea38814ed7b21d5774323a5766
Brian G <130684373+azurerazor@users.noreply.github.com>
2024-10-04 13:04:15 -0400

    fixes on expo

--- content/number-theory/Expo.h
+++ content/number-theory/Expo.h
@@ -2,8 +2,8 @@
  * Author: Brian Grana
  * Date: 2024-10-02
  * Source: own
- * Description: Given two integers b and p, computes b
- * raised to the power of p under modulo.
+ * Description: Given two integers b and p,
+ * computes $b^p$ under mod.
  * Time: $O(\log (p))$
  * Status: tested on https://cses.fi/problemset/task/1095/
  */
@@ -11,6 +11,7 @@
 
 const ll mod = 1e9+7;
 ll expo(long long b, long long p) {
+	// assert(p > 0);
 	ll ans = 1;
 	while (p > 0) {
 		if (p & 1) ans = (ans * b) % mod;
