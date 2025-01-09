a05a5f7d862f35ccd9dbc621b7fc6db19094409b
Sachin Sivakumar <ssk4988@gmail.com>
2025-01-09 16:45:07 -0500

    edits 2

--- content/graph/dsu.h
+++ content/graph/dsu.h
@@ -7,24 +7,24 @@
  * specific element is in (find(x)).
  * find(x) == find(y) if/only if x and y are in the same set
  * used to represent connected components in undirected graphs 
- * Time: amortized O(\alpha(N)) for find/join, O(N) Memory
- * \alpha(N) < 4 for all feasible input sizes
+ * Time: O(N) Memory, amortized O(\alpha(N)) for find/join
+ * , where \alpha(N) < 4 for all feasible input sizes
  * Status: Tested on codeforces edu dsu problems
  */
 struct dsu {
     vector<int> par, siz;
     dsu(int n) {
-        siz = par = vector<int>(n,1);
+        siz = par = vector<int>(n, 1);
         for (int i = 0; i < n; i++) par[i] = i;
     }
     int find(int a) {
         if (par[a] != a) par[a] = find(par[a]);
         return par[a];
     }
-    bool join(int u, int v){
+    bool join(int u, int v) {
         u = find(u); v = find(v);
         if (u == v) return false;
-        if (siz[u] < siz[v]) swap(u,v);
+        if (siz[u] < siz[v]) swap(u, v);
         siz[u] += siz[v];
         par[v] = u; 
         return true;
