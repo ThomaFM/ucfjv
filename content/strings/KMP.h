761370cbf1bf6962527dbb09719f90b6afb5483a
Sachin S <ssk4988@gmail.com>
2025-01-09 14:46:13 -0500

    improve readability and documentation on kmp (#44)
    * improve readability and documentation on kmp
    
    * update comment
    
    * update stress tests on kmp

--- content/strings/KMP.h
+++ content/strings/KMP.h
@@ -2,26 +2,28 @@
  * Author: Johan Sannemo
  * Date: 2016-12-15
  * License: CC0
- * Description: pi[x] computes the length of the longest prefix of s that ends at x,
- * other than s[0...x] itself (abacaba -> 0010123).
- * Can be used to find all occurrences of a string.
+ * Description: pi[x] computes the length of the longest 
+ * prefix of s that ends at x, other than s[0...x] itself
+ * (as an example, abacaba -> 0010123)
+ * match finds the starting indexes for all occurrences 
+ * of a specific pattern in s (overlaps allowed)
  * Time: O(n)
- * Status: Tested on kattis:stringmatching
+ * Status: Tested on https://cses.fi/problemset/task/1753/
  */
 #pragma once
 
-vector<int> pi(const string& s) {
-	vector<int> p(sz(s));
+vector<int> kmp(const string& s) {
+	vector<int> pi(sz(s));
 	for (int i = 1; i < sz(s); i++) {
-		int g = p[i-1];
-		while (g && s[i] != s[g]) g = p[g-1];
-		p[i] = g + (s[i] == s[g]);
+		pi[i] = pi[i-1];
+		while (pi[i] && s[i] != s[pi[i]]) pi[i] = pi[pi[i]-1];
+		if (s[i] == s[pi[i]]) pi[i]++;
 	}
-	return p;
+	return pi;
 }
 
 vector<int> match(const string& s, const string& pat) {
-	vector<int> p = pi(pat + '\0' + s), res;
+	vector<int> p = kmp(pat + '\0' + s), res;
 	for (int i = sz(p)-sz(s); i < sz(p); i++)
 		if (p[i] == sz(pat)) res.push_back(i - 2 * sz(pat));
 	return res;
