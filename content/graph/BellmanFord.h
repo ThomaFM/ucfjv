222692b236c0dfff25da92ab6eb961f4c89ea033
Brian G <130684373+azurerazor@users.noreply.github.com>
2025-09-17 22:05:29 -0400

    More graphs (#48)
    * finish floyd
    
    * create bfs
    
    * Update FloydWarshall.h
    
    * add BellmanFord
    
    * fix test link
    
    * tabbed
    
    * shorten the line wonkily
    
    * fix the guys

---
+++ content/graph/BellmanFord.h
@@ -0,0 +1,39 @@
+/**
+ * Author: Brian Grana
+ * Date: 2025-09-16
+ * Source: own
+ * Description: Given a weighted edge list of a graph
+ * and the number of nodes, determines the shortest path 
+ * from a given source node to all other nodes. Allows for 
+ * negative edge weights. Unreachable nodes are distance inf. 
+ * Nodes reachable via a negative cycle are distance -inf.
+ * Try to use Dijkstra if this TLE's.
+ * Time: $O(|V||E|)$
+ * Status: tested on https://cses.fi/problemset/task/1673/
+ */
+#pragma once
+
+const ll inf = 1e18;
+
+using edge = tuple<ll, int, int>;
+vector<ll> bellmanFord(vector<edge>& ed, int n, int s) {
+	vector<ll> dist(n, inf);
+	dist[s] = 0;
+	for (int i = 0; i < n; i++) {
+		for (auto [w, u, v] : ed) {
+			if (dist[u] == inf) continue;
+			if (dist[u] + w < dist[v]) {
+				if (i == n-1) dist[v] = -inf;
+				else dist[v] = dist[u] + w;
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (auto [w, u, v] : ed) {
+			if (dist[u] == -inf) dist[v] = -inf;
+		}
+	}
+
+	return dist;
+}
---
+++ content/graph/FloydWarshall.h
@@ -0,0 +1,27 @@
+/**
+ * Author: Brian Grana
+ * Date: 2025-01-10
+ * Source: own
+ * Description: Given a weighted adjacency matrix, returns
+ * a new weighted adjacency matrix M such that M[i][j] is
+ * equivalent to the weight of the shortest path starting
+ * at node i and ending at node j. Set any edges that don't
+ * exist to infinity.
+ * Time: $O(|V|^3)$
+ * Status: tested on https://cses.fi/problemset/task/1672
+ */
+#pragma once
+
+const ll inf = 1e18;
+
+vector<vector<ll>> floydWarshall(vector<vector<ll>> M) {
+	for (int i = 0; i < sz(M); i++) M[i][i] = 0;
+	for (int p = 0; p < sz(M); p++) {
+		for (int s = 0; s < sz(M); s++) {
+			for (int d = 0; d < sz(M); d++) {
+				M[s][d] = min(M[s][d], M[s][p] + M[p][d]);
+			}
+		}
+	}
+	return M;
+}
